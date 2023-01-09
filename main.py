cdfdee5e0cb8fbcea6e78256e00bc29e445b81c5
Yazan Amer <yazan.amer@progressoft.com>
2023-01-09 22:46:18 +0300

    main

--- main.py
+++ main.py
@@ -2,14 +2,3 @@ import pandas as pd
 
 
 data = pd.read_csv('resources/dataset/train.csv')
-image_class = 'class'
-image_path = 'image_path'
-name = 'name'
-x_max = 'xmax'
-x_min = 'xmin'
-y_max = 'ymax'
-y_min = 'ymin'
-
-if __name__ == '__main__':
-    print(data[name].unique())
-    print(data[image_class].unique())
